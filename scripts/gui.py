cc4a3d3d5d4432df1f8f211bec00a72b718c7ece
cmoutcalt <cmoutcalt@gmail.com>
2017-04-14 10:54:04 -0400

    base GUI fixed

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,6 +1,8 @@
-try:
-    from Tkinter import *
-except ImportError:
-    from tkinter import *
-top = Tkinter.Tk()
+from tkinter import *
+top = Tk()
+top.resizable(width=False, height=False)
+top.geometry('{}x{}'.format(500, 250)) #Formatted as W * H
+top.wm_title("Here Come DATBOI!")
+label = Label(top, text="Oh shit, what up?")
+label.pack()
 top.mainloop()
