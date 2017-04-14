6db7875450cddb1761a652ea6bf8ef2d9b2abbb0
cmoutcalt <cmoutcalt@gmail.com>
2017-04-14 11:09:06 -0400

    GUI additions

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,8 +1,20 @@
 from tkinter import *
+from tkinter import messagebox
+
+#Define the frame
 top = Tk()
 top.resizable(width=False, height=False)
 top.geometry('{}x{}'.format(500, 250)) #Formatted as W * H
 top.wm_title("Here Come DATBOI!")
+
+#Add a label to the frame
 label = Label(top, text="Oh shit, what up?")
 label.pack()
+
+#Add a button to the frame
+def killswitchEngage():
+    messagebox.showinfo("Execute Order 66", "It will be done, my lord")
+killswitch = Button(top, text="Order 66", command=killswitchEngage)
+killswitch.pack()
+
 top.mainloop()
