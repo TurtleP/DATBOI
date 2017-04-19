69b35fc9c69553f41c9266113278954b8f1d8d4a
cmoutcalt <cmoutcalt@gmail.com>
2017-04-19 12:03:48 -0400

    adding comments

--- source/encryption.py
+++ source/encryption.py
@@ -3,12 +3,15 @@ from passlib import pwd
 from gui import DATBOI
 from init import logger
 
+#Define or generate a password
 #password = pwd.genword(length=13, charset="ascii_62")
 password = "thisisapassword"
 print (password)
 
+#Hash the password, the function also generates and adds a salt
 hash = pbkdf2_sha256.hash(password)
 print (hash)
 
+#Attempt to verify the password
 print(pbkdf2_sha256.verify(password, hash))
 print(pbkdf2_sha256.verify("AYYYYY LMAO", hash))
