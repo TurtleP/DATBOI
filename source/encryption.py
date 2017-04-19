b114bf60af4d7722ef0efe948ca120afa3a584f2
cmoutcalt <cmoutcalt@gmail.com>
2017-04-19 12:01:07 -0400

    basic encryption works, now for design

--- source/encryption.py
+++ source/encryption.py
@@ -1,3 +1,14 @@
-from passlib import *
+from passlib.hash import pbkdf2_sha256
+from passlib import pwd
 from gui import DATBOI
 from init import logger
+
+#password = pwd.genword(length=13, charset="ascii_62")
+password = "thisisapassword"
+print (password)
+
+hash = pbkdf2_sha256.hash(password)
+print (hash)
+
+print(pbkdf2_sha256.verify(password, hash))
+print(pbkdf2_sha256.verify("AYYYYY LMAO", hash))
