0d71cf8d9ade177d2a41396fa53c8ac54fa1b712
cmoutcalt <cmoutcalt@gmail.com>
2017-04-19 21:12:51 -0400

    few more minor cleanups

--- source/encryption.py
+++ source/encryption.py
@@ -2,17 +2,8 @@ from passlib.hash import pbkdf2_sha512
 from passlib import pwd
 
 """#Define or generate a password
-#password = pwd.genword(length=13, charset="ascii_62")
-password = "thisisapasswordlol"
-print (password)
-
-#Hash the password, the function also generates and adds a salt
-hash = pbkdf2_sha512.hash(password)
-print (hash)
-
-#Attempt to verify the password
-print(pbkdf2_sha512.verify(password, hash))
-print(pbkdf2_sha512.verify("AYYYYY LMAO", hash))"""
+password = pwd.genword(length=13, charset="ascii_62")"""
+hash = None
 
 def setPassword(password):
     if len(password) >= 14 and len(password) <= 20:
